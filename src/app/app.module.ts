9dec2ee26dd16f8d035d2b4ee2ca2c7016cc3078
Felipe-Marin <felipmarin5@gmail.com>
2017-12-31 14:42:08 -0200

    Incluido SheetProvider
    Incluid provider responsável por pegar os dados da programação da
    planilha no Google Docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ProgramacaoPage } from './../pages/programacao/programacao';
+
+import { SheetProvider } from '../providers/sheet/sheet';
+import { HttpClientModule } from '@angular/common/http';
 
 //import { StatusBar } from '@ionic-native/status-bar';
 //import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,10 +21,12 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProgramacaoPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,11 +35,13 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProgramacaoPage
   ],
   providers: [
     //StatusBar,
     //SplashScreen,
+    SheetProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/providers/sheet/sheet.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the SheetProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SheetProvider {
+  sheetURL: string;
+
+  constructor(public http: HttpClient) {
+    this.sheetURL = 'https://spreadsheets.google.com/feeds/list/1GaNzOyrRfdY5MIrUSBY4_TnV0oEc3nG-bNwdbzKUJu4/od6/public/values?alt=json';
+  }
+
+  getProgramacao(){
+    //this.http.get<SheetResponse>(this.sheetURL).subscribe(data => {return data.feed});
+    return this.http.get(this.sheetURL);
+  }
+
+}
