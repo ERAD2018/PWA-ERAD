d93cce99f56075a1dd5e2eecf40f481d17cd4af6
Felipe-Marin <felipmarin5@gmail.com>
2018-01-03 22:29:14 -0200

    Select data e local
    Adicionado elementos select pra filtrar data e local da lista do evento

--- src/pages/programacao/programacao.ts
+++ src/pages/programacao/programacao.ts
@@ -18,12 +18,17 @@ export class ProgramacaoPage {
   
   listaProgramacao: any;
   listaBkp: any;
+  selectDataOptions: any;
+  selectLocalOptions: any;
+  selectedData: string;
+  selectedLocal: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private sheetProvider: SheetProvider) {
   }
 
   ngOnInit(){
     this.getProgramacao();
+
   }
 
   ionViewDidLoad() {
@@ -37,7 +42,8 @@ export class ProgramacaoPage {
       var dataResponse = <SheetResponse>data;
       this.listaProgramacao = dataResponse.feed['entry'];
       this.listaBkp = this.listaProgramacao;
-      console.log(this.listaProgramacao);
+      this.setDataSelect();
+      this.setLocalSelect();
     },
       err => {
         console.log("Erro.");
@@ -61,10 +67,53 @@ export class ProgramacaoPage {
     this.navCtrl.push(DetalhePage, {detalhes: item});
   }
 
+  setDataSelect(){
+    this.selectDataOptions = [];
+    this.listaProgramacao.forEach(element => {
+      if(this.selectDataOptions.indexOf(element.gsx$data.$t) == -1){
+        console.log(element.gsx$data.$t);
+        this.selectDataOptions.push(element.gsx$data.$t);
+      }
+    });
+    this.selectedData = 'Todos';
+    console.log(this.selectDataOptions);
+  }
+
+  setLocalSelect(){
+    this.selectLocalOptions = [];
+    this.listaProgramacao.forEach(element => {
+      if(this.selectLocalOptions.indexOf(element.gsx$local.$t) == -1 && element.gsx$local.$t != ''){
+        console.log(element.gsx$local.$t);
+        this.selectLocalOptions.push(element.gsx$local.$t);
+      }
+    });
+    this.selectedLocal = 'Todos';
+    console.log(this.selectLocalOptions);
+  }
+
+  showFilter(){
+
+  }
+
+  filterProgramacao(ev: any){
+    console.log('Data:' + this.selectedData + ' - Local: ' + this.selectedLocal);   
+    this.listaProgramacao = this.listaBkp;
+    if(this.selectedData != 'Todos'){
+      this.listaProgramacao = this.listaProgramacao.filter((item) => {
+        return(item.gsx$data.$t.toLowerCase().indexOf(this.selectedData.toLowerCase()) > -1);
+      })
+    }
+    if(this.selectedLocal != 'Todos'){
+      this.listaProgramacao = this.listaProgramacao.filter((item) => {
+        return(item.gsx$local.$t.toLowerCase().indexOf(this.selectedLocal.toLowerCase()) > -1);
+      })
+    }
+  }
+
 }
 
   
 
 interface SheetResponse {
-  feed: string;
+  feed: Object;
 }
