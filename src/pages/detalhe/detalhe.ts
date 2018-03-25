934ba6122265da2ea73fd2c94983e6f6fa1f23a4
Felipe-Marin <felipmarin5@gmail.com>
2018-03-25 20:57:30 -0300

    Adicionado botão de favorito na página de detalhe
    Adicionado botão para adicionar e remover item dos favoritos na página
    de detalhes da programação.

--- src/pages/detalhe/detalhe.ts
+++ src/pages/detalhe/detalhe.ts
@@ -1,5 +1,6 @@
+import { UserDataProvider } from './../../providers/user-data/user-data';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the DetalhePage page.
@@ -14,9 +15,9 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class DetalhePage {
 
-  detalhes:any;
+  detalhes: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController, private userProvider: UserDataProvider) {
     this.detalhes = this.navParams.data.detalhes;
   }
 
@@ -24,13 +25,30 @@ export class DetalhePage {
     console.log('ionViewDidLoad DetalhePage');
   }
 
-  goBack(){
-    if (this.navCtrl.canGoBack()){
+  goBack() {
+    if (this.navCtrl.canGoBack()) {
       this.navCtrl.pop();
     }
   }
 
-  openFile(url: string){
+  openFile(url: string) {
     window.open(url);
+  }
+
+  setFavorite(item: any) {
+    item.favorito = !item.favorito;
+    let message = "";
+    if (item.favorito) {
+      message = "Adicionado aos favoritos.";
+      this.userProvider.setFavorito(item.id);
+    } else {
+      message = "Removido dos favoritos.";
+      this.userProvider.removeFavorito(item.id);
+    }
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 1000
+    });
+    toast.present();
   }
 }
